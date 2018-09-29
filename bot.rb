71e2ec4a8143a1bb72de28663f3ec1df0b12ee8a
Diego Camacho <hackvan@gmail.com>
2018-09-29 12:30:35 -0500

    Add issues command to telegram bot for list the issues from a repository

--- bot.rb
+++ bot.rb
@@ -1,8 +1,9 @@
 require 'telegram_bot'
+require_relative 'github'
 
-token = '639592041:AAGemO4NNgl42xgXYKXplwUz5qnqqToUNVg'
+telegram_token = '639592041:AAGemO4NNgl42xgXYKXplwUz5qnqqToUNVg'
 
-bot = TelegramBot.new(token: token)
+bot = TelegramBot.new(token: telegram_token)
 
 bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
@@ -11,9 +12,16 @@ bot.get_updates(fail_silently: true) do |message|
   message.reply do |reply|
     case command
     when /start/i
-      reply.text = "Todo lo que puedo hacer es decir Hola. Prueba el comando /greet"
-    when /greet/i
-      reply.text = "Hola, #{message.from.first_name}."
+      reply.text = "Hola #{message.from.first_name}, para iniciar puedes utilizar el comando /setup."
+    when /setup/i
+      reply.text = "Por favor indica el @usuario de github:"
+    when /issues/i
+      issues  = GitHubWrapper::GitHubConnector.new
+      reply.text = "Incidentes para: @#{issues.username}/#{issues.repository}:\n\n"
+      issues.get_issues.each do |issue|
+        reply.text << "  id: #{issue.number}\n  #{issue.body}\n"
+        reply.text << "  -----\n\n"
+      end
     else
       reply.text = "No tengo idea de lo que el comando #{command.inspect} significa."
     end
